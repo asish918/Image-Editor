da2c798b14843875f638045419f9e2e0e174f72a
Asish Mahapatra <asishmahapatra918@gmail.com>
2022-10-06 11:18:35 +0530

    Implemented all the functions of App Bar

--- lib/screens/edit_image_screen.dart
+++ lib/screens/edit_image_screen.dart
@@ -31,8 +31,13 @@ class _EditImageScreenState extends EditImageViewModel {
                     right: texts[i].right,
                     top: texts[i].top,
                     child: GestureDetector(
-                      onLongPress: () => debugPrint("Long Pressed!!"),
-                      onTap: () => debugPrint("Tapped!!"),
+                      onLongPress: () {
+                        setState(() {
+                          currentIndex = i;
+                          removeText(context);
+                        });
+                      },
+                      onTap: () => setCurrentIndex(context, i),
                       child: Draggable(
                         feedback: ImageText(textInfo: texts[i]),
                         child: ImageText(
@@ -86,7 +91,7 @@ class _EditImageScreenState extends EditImageViewModel {
             scrollDirection: Axis.horizontal,
             children: [
               IconButton(
-                onPressed: () {},
+                onPressed: () => increaseFontSize(),
                 icon: Icon(
                   Icons.add,
                   color: Colors.black,
@@ -94,7 +99,7 @@ class _EditImageScreenState extends EditImageViewModel {
                 tooltip: "Increase Font Size",
               ),
               IconButton(
-                onPressed: () {},
+                onPressed: () => decreaseFontSize(),
                 icon: Icon(
                   Icons.remove,
                   color: Colors.black,
@@ -102,7 +107,7 @@ class _EditImageScreenState extends EditImageViewModel {
                 tooltip: "Decrease Font Size",
               ),
               IconButton(
-                onPressed: () {},
+                onPressed: () => alignLeft(),
                 icon: Icon(
                   Icons.format_align_left,
                   color: Colors.black,
@@ -110,7 +115,7 @@ class _EditImageScreenState extends EditImageViewModel {
                 tooltip: "Align Left",
               ),
               IconButton(
-                onPressed: () {},
+                onPressed: () => alignCenter(),
                 icon: Icon(
                   Icons.format_align_center,
                   color: Colors.black,
@@ -118,7 +123,7 @@ class _EditImageScreenState extends EditImageViewModel {
                 tooltip: "Align Center",
               ),
               IconButton(
-                onPressed: () {},
+                onPressed: () => alignRight(),
                 icon: Icon(
                   Icons.format_align_right,
                   color: Colors.black,
@@ -134,7 +139,7 @@ class _EditImageScreenState extends EditImageViewModel {
                 tooltip: "Save Image",
               ),
               IconButton(
-                onPressed: () {},
+                onPressed: () => boldText(),
                 icon: Icon(
                   Icons.format_bold,
                   color: Colors.black,
@@ -142,7 +147,7 @@ class _EditImageScreenState extends EditImageViewModel {
                 tooltip: "Bold",
               ),
               IconButton(
-                onPressed: () {},
+                onPressed: () => italicText(),
                 icon: Icon(
                   Icons.format_italic,
                   color: Colors.black,
@@ -150,7 +155,7 @@ class _EditImageScreenState extends EditImageViewModel {
                 tooltip: "Italic",
               ),
               IconButton(
-                onPressed: () {},
+                onPressed: () => addLinesToText(),
                 icon: Icon(
                   Icons.space_bar,
                   color: Colors.black,
@@ -160,7 +165,7 @@ class _EditImageScreenState extends EditImageViewModel {
               Tooltip(
                 message: "Red",
                 child: GestureDetector(
-                  onTap: () {},
+                  onTap: () => changeTextColor(Colors.red),
                   child: const CircleAvatar(
                     backgroundColor: Colors.red,
                   ),
@@ -172,7 +177,7 @@ class _EditImageScreenState extends EditImageViewModel {
               Tooltip(
                 message: "White",
                 child: GestureDetector(
-                  onTap: () {},
+                  onTap: () => changeTextColor(Colors.white),
                   child: const CircleAvatar(
                     backgroundColor: Colors.white,
                   ),
@@ -184,7 +189,7 @@ class _EditImageScreenState extends EditImageViewModel {
               Tooltip(
                 message: "Black",
                 child: GestureDetector(
-                  onTap: () {},
+                  onTap: () => changeTextColor(Colors.black),
                   child: const CircleAvatar(
                     backgroundColor: Colors.black,
                   ),
@@ -196,7 +201,7 @@ class _EditImageScreenState extends EditImageViewModel {
               Tooltip(
                 message: "Blue",
                 child: GestureDetector(
-                  onTap: () {},
+                  onTap: () => changeTextColor(Colors.blue),
                   child: const CircleAvatar(
                     backgroundColor: Colors.blue,
                   ),
@@ -208,7 +213,7 @@ class _EditImageScreenState extends EditImageViewModel {
               Tooltip(
                 message: "Yellow",
                 child: GestureDetector(
-                  onTap: () {},
+                  onTap: () => changeTextColor(Colors.yellow),
                   child: const CircleAvatar(
                     backgroundColor: Colors.yellow,
                   ),
@@ -220,7 +225,7 @@ class _EditImageScreenState extends EditImageViewModel {
               Tooltip(
                 message: "Green",
                 child: GestureDetector(
-                  onTap: () {},
+                  onTap: () => changeTextColor(Colors.green),
                   child: const CircleAvatar(
                     backgroundColor: Colors.green,
                   ),
@@ -232,7 +237,7 @@ class _EditImageScreenState extends EditImageViewModel {
               Tooltip(
                 message: "Orange",
                 child: GestureDetector(
-                  onTap: () {},
+                  onTap: () => changeTextColor(Colors.orange),
                   child: const CircleAvatar(
                     backgroundColor: Colors.orange,
                   ),
@@ -244,7 +249,7 @@ class _EditImageScreenState extends EditImageViewModel {
               Tooltip(
                 message: "Pink",
                 child: GestureDetector(
-                  onTap: () {},
+                  onTap: () => changeTextColor(Colors.pink),
                   child: const CircleAvatar(
                     backgroundColor: Colors.pink,
                   ),
--- lib/widgets/edit_image_view_model.dart
+++ lib/widgets/edit_image_view_model.dart
@@ -5,6 +5,100 @@ import '../models/text_info.dart';
 
 abstract class EditImageViewModel extends State<EditImageScreen> {
   List<TextInfo> texts = [];
+  int currentIndex = 0;
+
+  removeText(BuildContext context){
+    setState(() {
+      texts.removeAt(currentIndex);
+    });
+    ScaffoldMessenger.of(context).showSnackBar(
+      SnackBar(
+        content: Text(
+          "Deleted",
+          style: TextStyle(fontSize: 16.0),
+        ),
+      ),
+    );
+  }
+
+  setCurrentIndex(BuildContext context, index) {
+    setState(() {
+      currentIndex = index;
+    });
+    ScaffoldMessenger.of(context).showSnackBar(
+      SnackBar(
+        content: Text(
+          "Selected For Styling",
+          style: TextStyle(fontSize: 16.0),
+        ),
+      ),
+    );
+  }
+
+  changeTextColor(Color color) {
+    setState(() {
+      texts[currentIndex].color = color;
+    });
+  }
+
+  increaseFontSize() {
+    setState(() {
+      texts[currentIndex].fontSize += 2;
+    });
+  }
+
+  decreaseFontSize() {
+    setState(() {
+      texts[currentIndex].fontSize -= 2;
+    });
+  }
+
+  alignLeft() {
+    setState(() {
+      texts[currentIndex].textAlign = TextAlign.left;
+    });
+  }
+
+  alignRight() {
+    setState(() {
+      texts[currentIndex].textAlign = TextAlign.right;
+    });
+  }
+
+  alignCenter() {
+    setState(() {
+      texts[currentIndex].textAlign = TextAlign.center;
+    });
+  }
+
+  boldText() {
+    setState(() {
+      if (texts[currentIndex].fontWeight == FontWeight.bold)
+        texts[currentIndex].fontWeight = FontWeight.normal;
+      else
+        texts[currentIndex].fontWeight = FontWeight.bold;
+    });
+  }
+
+  italicText() {
+    setState(() {
+      if (texts[currentIndex].fontStyle == FontStyle.italic)
+        texts[currentIndex].fontStyle = FontStyle.normal;
+      else
+        texts[currentIndex].fontStyle = FontStyle.italic;
+    });
+  }
+
+  addLinesToText() {
+    setState(() {
+      if (texts[currentIndex].text.contains("\n"))
+        texts[currentIndex].text =
+            texts[currentIndex].text.replaceAll("\n", " ");
+      else
+        texts[currentIndex].text =
+            texts[currentIndex].text.replaceAll(" ", "\n");
+    });
+  }
 
   TextEditingController textEditingController = TextEditingController();
   TextEditingController creatorText = TextEditingController();
